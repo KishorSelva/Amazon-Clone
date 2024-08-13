7c091b9926a488b6934131969cf4c8c63d152c06
KishorSelva <kishorselvakumar04@gmail.com>
2024-08-13 22:08:11 +0530

    Cenerated checkout page html using javascript(but radio buttons are broken)

---
+++ scripts/checkout.js
@@ -0,0 +1,102 @@
+//import {cart} from '../data/cart.js'
+import { products } from '../data/products.js'
+
+let cart = [{
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 1
+}, {
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2
+}]
+
+let productsText = '';
+
+cart.forEach((element) => {
+    let matchingObject
+    products.forEach((object) => {
+        if(element.productId === object.id) {
+            matchingObject = object;
+        } 
+    })
+    productsText += `
+        <div class="cart-item-container">
+            <div class="delivery-date">
+              Delivery date: Tuesday, June 21
+            </div>
+
+            <div class="cart-item-details-grid">
+              <img class="product-image"
+                src="${matchingObject.image}">
+
+              <div class="cart-item-details">
+                <div class="product-name">
+                  ${matchingObject.name}
+                </div>
+                <div class="product-price">
+                  $${(matchingObject.priceCents / 100).toFixed(2)}
+                </div>
+                <div class="product-quantity">
+                  <span>
+                    Quantity: <span class="quantity-label">${element.quantity}</span>
+                  </span>
+                  <span class="update-quantity-link link-primary">
+                    Update
+                  </span>
+                  <span class="delete-quantity-link link-primary">
+                    Delete
+                  </span>
+                </div>
+              </div>
+
+              <div class="delivery-options">
+                <div class="delivery-options-title">
+                  Choose a delivery option:
+                </div>
+                <div class="delivery-option">
+                  <input type="radio" checked
+                    class="delivery-option-input"
+                    name="delivery-option-1">
+                  <div>
+                    <div class="delivery-option-date">
+                      Tuesday, June 21
+                    </div>
+                    <div class="delivery-option-price">
+                      FREE Shipping
+                    </div>
+                  </div>
+                </div>
+                <div class="delivery-option">
+                  <input type="radio"
+                    class="delivery-option-input"
+                    name="delivery-option-1">
+                  <div>
+                    <div class="delivery-option-date">
+                      Wednesday, June 15
+                    </div>
+                    <div class="delivery-option-price">
+                      $4.99 - Shipping
+                    </div>
+                  </div>
+                </div>
+                <div class="delivery-option">
+                  <input type="radio"
+                    class="delivery-option-input"
+                    name="delivery-option-1">
+                  <div>
+                    <div class="delivery-option-date">
+                      Monday, June 13
+                    </div>
+                    <div class="delivery-option-price">
+                      $9.99 - Shipping
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+    `
+})
+
+let cartDisplayElement = document.querySelector('.js-order-summary');
+
+cartDisplayElement.innerHTML = productsText

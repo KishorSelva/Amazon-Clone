56010d4e80027c887a636eedb0e72683f11b250f
KishorSelva <kishorselvakumar04@gmail.com>
2024-09-08 22:33:42 +0530

    removed some practice fluff code

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -6,12 +6,6 @@ import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js'
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js'
 import { renderPaymentSummary } from './paymentSummary.js';
 
-const today = dayjs()
-const deliveryDate = today.add(7, 'days');
-console.log(deliveryDate.format('dddd, MMMM D'))
-
-hello()
-
 export function renderOrderSummary() {
 
   let productsText = '';

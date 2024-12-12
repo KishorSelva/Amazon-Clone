6b36b33243cecc74b2a617b9d5b71e6701e64b37
KishorSelva <kishorselvakumar04@gmail.com>
2024-12-12 07:53:18 +0530

    Added meesage when no products match the search element

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -33,59 +33,69 @@ export function renderProductsGrid() {
 
   console.log(localProducts);
 
-  localProducts.forEach((object) => {
-      innerContent  += `
-          <div class="product-container">
-            <div class="product-image-container">
-              <img class="product-image" src="${object.image}">
-            </div>
-
-            <div class="product-name limit-text-to-2-lines">
-              ${object.name}
-            </div>
-
-            <div class="product-rating-container">
-              <img class="product-rating-stars"
-                src="${object.getStarsUrl()}">
-              <div class="product-rating-count link-primary">
-                ${object.rating.count}
+  if (localProducts.length == 0) {
+    innerContent = `
+      <div style="font-size: 20px; margin: 10px position: float">
+        No products matches your search.
+      </div>
+    `
+  } else {
+      localProducts.forEach((object) => {
+        innerContent  += `
+            <div class="product-container">
+              <div class="product-image-container">
+                <img class="product-image" src="${object.image}">
               </div>
+
+              <div class="product-name limit-text-to-2-lines">
+                ${object.name}
+              </div>
+
+              <div class="product-rating-container">
+                <img class="product-rating-stars"
+                  src="${object.getStarsUrl()}">
+                <div class="product-rating-count link-primary">
+                  ${object.rating.count}
+                </div>
+              </div>
+
+              <div class="product-price">
+                ${object.getPrice()}
+              </div>
+
+              <div class="product-quantity-container">
+                <select class="js-quantity-selector-${object.id}">
+                  <option selected value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                  <option value="6">6</option>
+                  <option value="7">7</option>
+                  <option value="8">8</option>
+                  <option value="9">9</option>
+                  <option value="10">10</option>
+                </select>
+              </div>
+
+              ${object.extraInfoHTML()}
+
+              <div class="product-spacer"></div>
+
+              <div class="added-to-cart js-added-message-${object.id}">
+                <img src="images/icons/checkmark.png">
+                Added
+              </div>
+
+              <button class="add-to-cart-button js-add-to-cart-button button-primary" data-product-id="${object.id}">
+                Add to Cart
+              </button>
             </div>
+        `
+    })
+  }
 
-            <div class="product-price">
-              ${object.getPrice()}
-            </div>
 
-            <div class="product-quantity-container">
-              <select class="js-quantity-selector-${object.id}">
-                <option selected value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-              </select>
-            </div>
-
-            ${object.extraInfoHTML()}
-
-            <div class="product-spacer"></div>
-
-            <div class="added-to-cart js-added-message-${object.id}">
-              <img src="images/icons/checkmark.png">
-              Added
-            </div>
-
-            <button class="add-to-cart-button js-add-to-cart-button button-primary" data-product-id="${object.id}">
-              Add to Cart
-            </button>
-          </div>
-      `
-  })
 
   productsDisplay.innerHTML = innerContent;
 
